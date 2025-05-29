3167d85b36b242710cf0eb8581a3e6679af0ba8b
Kojiro Futamura <gumob.dev@gmail.com>
2025-05-29 17:55:14 +0900

    Update configuration files for Prettier and TypeScript - Change trailingComma setting in prettier.config.js from 'es2020' to 'es5' - Update TypeScript target in tsconfig.json from 'ES2020' to 'es5'

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,7 @@ export default {
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: 'es2020',
+  trailingComma: 'es5',
   printWidth: 160,
   bracketSpacing: true,
   arrowParens: 'avoid',
