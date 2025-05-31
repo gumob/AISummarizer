e1825c3aabc4e61e0296ba974ac6fb81a1e11b11
Kojiro Futamura <gumob.dev@gmail.com>
2025-05-31 11:49:58 +0900

    Update TabBehavior enum for clarity - Rename CURRENT_PAGE to CURRENT_TAB for better terminology

--- src/types/TabBahavior.ts
+++ src/types/TabBahavior.ts
@@ -1,5 +1,5 @@
 export enum TabBehavior {
-  CURRENT_PAGE = 'Current Page',
+  CURRENT_TAB = 'Current Tab',
   NEW_TAB = 'New Tab',
   NEW_PRIVATE_TAB = 'New Private Tab',
   NEW_INCOGNITO_TAB = 'New Incognito Tab',
