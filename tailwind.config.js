e807d027e3f110a1ea8d9f9a6c87951ee25c34d8
Kojiro Futamura <gumob.dev@gmail.com>
2025-06-11 22:55:41 +0900

    Update font sizes in tailwind.config.js to include !important for improved specificity in styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,19 @@ export default {
   theme: {
     extend: {
       fontSize: {
-        xs: '0.5rem', // 8px
-        sm: '0.625rem', // 10px
-        base: '0.75rem', // 12px
-        lg: '0.875rem', // 14px
-        xl: '1rem', // 16px
-        '2xl': '1.125rem', // 18px
-        '3xl': '1.5rem', // 24px
-        '4xl': '2rem', // 32px
-        '5xl': '2.5rem', // 40px
-        '6xl': '3rem', // 48px
-        '7xl': '3.5rem', // 56px
-        '8xl': '4rem', // 64px
-        '9xl': '4.5rem', // 72px
+        xs: '0.5rem !important', // 8px
+        sm: '0.625rem !important', // 10px
+        base: '0.75rem !important', // 12px
+        lg: '0.875rem !important', // 14px
+        xl: '1rem !important', // 16px
+        '2xl': '1.125rem !important', // 18px
+        '3xl': '1.5rem !important', // 24px
+        '4xl': '2rem !important', // 32px
+        '5xl': '2.5rem !important', // 40px
+        '6xl': '3rem !important', // 48px
+        '7xl': '3.5rem !important', // 56px
+        '8xl': '4rem !important', // 64px
+        '9xl': '4.5rem !important', // 72px
       },
     },
   },
