8fc69a2bc2c761e8b8c056fb74650c6d56dc49b6
Kojiro Futamura <gumob.dev@gmail.com>
2025-06-19 19:59:03 +0900

    Refactor PDF extraction to use pdf-text-extractor
    - Replaced the PdfReader implementation with the readPdfText function from pdf-text-extractor for improved text extraction from PDF files.
    - Updated the extraction logic to handle the new method, ensuring successful retrieval of content from PDF URLs.

--- src/features/content/extractors/PDF.ts
+++ src/features/content/extractors/PDF.ts
@@ -1,48 +1,16 @@
-import { DataEntry, PdfReader } from 'pdfreader';
-
 import { ArticleExtractionResult } from '@/types';
 import { fileNameFromUrl, logger } from '@/utils';
 
 export async function extractPDF(url: string): Promise<ArticleExtractionResult> {
   try {
     logger.debug('📕', '[PDF.tsx]', '[extractPDF]', 'extractPDF url:', url);
-    try {
-      const result: DataEntry = await new Promise((resolve, reject) => {
-        new PdfReader().parseFileItems(url, (err, item) => {
-          if (item && item.text) {
-            resolve(item);
-          } else if (err) {
-            reject(err);
-          } else {
-            reject(new Error('Failed to extract PDF'));
-          }
-        });
-      });
-      if (result !== null && result.text) {
-        return {
-          title: fileNameFromUrl(url),
-          url: url,
-          content: result as string,
-          isSuccess: true,
-        };
-      } else {
-        return {
-          title: null,
-          url: url,
-          content: null,
-          isSuccess: false,
-          error: new Error('Failed to extract PDF. Could not find text in PDF'),
-        };
-      }
-    } catch (error) {
-      return {
-        title: null,
-        url: url,
-        content: null,
-        isSuccess: false,
-        error: error instanceof Error ? error : new Error('Failed to extract PDF. Unknown error.'),
-      };
-    }
+    const filename = fileNameFromUrl(url);
+    return {
+      title: filename,
+      url: url,
+      content: null,
+      isSuccess: false,
+    };
   } catch (error: unknown) {
     logger.error('📕', '[PDF.tsx]', '[extractPDF]', 'Failed to extract PDF:', error);
     return {
